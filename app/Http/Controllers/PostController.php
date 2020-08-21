ce42d4d9106eae3d892cc1d3f255c4229ea0a99e
海野裕 <u.hiroshi19920103@gmail.com>
2020-08-21 22:46:40 +0900

    2nd commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,13 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    
     //
-
 }
---
+++ app/Relation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use App\Relation;
+use Illuminate\Database\Eloquent\Model;
+
+class Relation extends Model
+{
+    //
+}
